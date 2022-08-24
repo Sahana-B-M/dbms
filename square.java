d119e449d923e990b6d7701f4deb0934c029b0bb
Sahana-B-M <100989312+Sahana-B-M@users.noreply.github.com>
2022-08-24 16:04:08 +0530

    Add files via upload

---
+++ square.java
@@ -0,0 +1,11 @@
+package shape;
+
+public class square {
+    float side;
+    public void getData(Float  temp){
+        side=temp;
+    }
+    public double area(){
+        return(side*side);
+    }
+}
